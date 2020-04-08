9c86f0ba9b948fbcf4f535224d8737f5523ed424
Bhuvan Karuturi <bhuvankaruturi123@gmail.com>
2020-04-08 17:57:14 -0500

    added 50 states and usa

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ var height = 600;
 var country = "US";
 var stat = "confirmed";
 var dailyReportBaseUri = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/";
+// color values
+var colorValues = {
+    deaths: ["#facaca", "#f70505"],
+    confirmed: ["#cdd8fa", "#426ff5"]
+};
+var dataCSV = {};
+var dataRead = false;
 
 function addZero(n) {
     return n > 10 ? '' + n : '0' + n;
@@ -17,6 +24,14 @@ function getCSVFileName () {
     return dailyReportBaseUri + formatedDate + '.csv';
 }
 
+function capitalize(s) {
+    return s.charAt(0).toUpperCase() + currState.slice(1);
+}
+
+function transformName(s) {
+    return s.toLowerCase().split(' ').join('-');
+}
+
 var currState = d3.select('#state').property('value');
 
 d3.select('#state')
@@ -24,15 +39,39 @@ d3.select('#state')
         let newState = d3.select('#state').property('value');
         if (currState && currState !== newState) {
             currState = newState;
-            init(currState);
+            if (dataRead) buildNewMap(currState);
+            else init(currState)
         }
     });
 
-if (currState) init(currState);
-function capitalize(s) {
-    return s.charAt(0).toUpperCase() + currState.slice(1);
+populateOptions('texas');
+
+async function init(currState) {
+    let promise = getCoronaData(getCSVFileName());
+    let result = await promise;
+    if (result) {
+        dataRead = true;
+        buildNewMap(currState);
+    }
 }
-function init(currState) {
+
+function populateOptions(defaultMap='texas') {
+    let keys = Object.keys(states);
+    let stateList = [];
+    for (let key of keys) {
+        stateList.push({name: states[key], id: transformName(states[key])});
+    }
+    d3.select("#state").selectAll("option").data(stateList)
+            .enter().append("option")
+            .property('value', d => d.id)
+            .property('textContent', d => d.name)
+            .property('selected', d => d.id === defaultMap);
+    currState = defaultMap;
+    init(currState);
+}
+
+async function getCoronaData(dataUri) {
+    let promise = new Promise((resolve, reject) => {
     d3.queue().defer(d3.csv, dataUri, (row, i, header) => {
         return {
             country: row.Country_Region,
@@ -43,78 +82,117 @@ function init(currState) {
             recovered: +row.Recovered,
         }
     })
-    .defer(d3.json, `./maps/${currState}.json`)
-    .await((error, coronaData, topoData) => {
-        if (error) console.log(error);
-        else {
-            coronaData = coronaData.filter(row => row.country === country && row.state === capitalize(currState));
-            let stateDetails = {
-                name: capitalize(currState),
-                confirmed: 0,
-                deaths: 0,
-                recovered: 0
-            };
-            coronaData.forEach(c => {
-                topoData.objects[currState+'-counties'].geometries.forEach(g => {
-                    if (g.properties.NAME === c.county) {
-                        g.properties.confirmed = c.confirmed;
-                        g.properties.deaths = c.deaths;
-                        g.properties.recovered = c.recovered;
+    .await((error, coronaData) => {
+        if (error) {
+            console.log(error);
+            reject(0);
+        }
+        else { 
+            let allStates = 'all-states';
+            coronaData.forEach(row => {
+                if (row.country === country) {
+                    let state = transformName(row.state);
+                    if (dataCSV[state]) {
+                        dataCSV[state].info.confirmed += row.confirmed;
+                        dataCSV[state].info.deaths += row.deaths;
+                        dataCSV[state].info.recovered += row.recovered;
+                        dataCSV[state].regions.push({
+                            name: row.county, 
+                            confirmed: row.confirmed,
+                            deaths: row.deaths,
+                            recovered: row.recovered
+                        });
+                    } else {
+                        dataCSV[state] = {};
+                        dataCSV[state].info = {
+                            name: row.state, 
+                            confirmed: row.confirmed,
+                            deaths: row.deaths,
+                            recovered: row.recovered 
+                        };
+                        dataCSV[state].regions = [];
+                        dataCSV[state].regions.push({
+                                                name: row.county, 
+                                                confirmed: row.confirmed,
+                                                deaths: row.deaths,
+                                                recovered: row.recovered 
+                                            });
                     }
-                })
-                stateDetails.confirmed += c.confirmed;
-                stateDetails.deaths += c.deaths;
-                stateDetails.recovered += c.recovered;
+                    if (dataCSV[allStates]) {
+                        dataCSV[allStates].info.confirmed += row.confirmed;
+                        dataCSV[allStates].info.deaths += row.deaths;
+                        dataCSV[allStates].info.recovered += row.recovered;
+                        dataCSV[allStates].regions.push(dataCSV[state].info);
+                    } else {
+                        dataCSV[allStates] = {};
+                        dataCSV[allStates].info = {
+                            name: row.state, 
+                            confirmed: row.confirmed,
+                            deaths: row.deaths,
+                            recovered: row.recovered 
+                        };
+                        dataCSV[allStates].info.name = country;
+                        dataCSV[allStates].info.confirmed = row.confirmed;
+                        dataCSV[allStates].info.deaths = row.deaths;
+                        dataCSV[allStates].info.recovered = row.recovered;
+                        dataCSV[allStates].regions = [dataCSV[state].info];
+                    }
+                }
             });
-            var topology = topojson.feature(topoData, topoData.objects[currState + '-counties']);
-            d3.select("select")
-				.on('change', function(){
-					drawMap(coronaData, topology);
-                });
-            d3.select('#map').selectAll('svg').selectAll('.map').remove();
-            drawMap(coronaData, topology);
-            showStateInfo(stateDetails);
+            resolve(1);
         }
     });
+    });
+    return promise;
 }
 
-const drawMap = function (coronaData, topology, stateDetails) {
+function buildNewMap(currState) {
+    d3.queue().defer(d3.json, `./maps/${currState}.json`)
+        .await((error, topoData) => {
+            if (error) console.log(error);
+            else {
+                let objectName = currState;
+                if (currState !== "all-states") objectName += '-counties';
+                dataCSV[currState].regions.forEach(c => {
+                    topoData.objects[objectName].geometries.forEach(g => {
+                        if (g.properties.NAME === c.name) {
+                            g.properties.confirmed = c.confirmed;
+                            g.properties.deaths = c.deaths;
+                            g.properties.recovered = c.recovered;
+                        }
+                    })
+                });
+                var topology = topojson.feature(topoData, topoData.objects[objectName]);
+                d3.select("select")
+                    .on('change', function(){
+                        drawMapWithStat();
+                    });
+                d3.select('#map').selectAll('svg').selectAll('.map').remove();
+                drawMap(topology);
+                showInfo(dataCSV[currState].info);
+            }
+        });
+}
+
+const drawMap = function (topology) {
     var stat = d3.select('#stat').property('value');
     var prevSelected = null;
-    // color scale
-    var colorValues = {
-        deaths: ["#facaca", "#f70505"],
-        confirmed: ["#cdd8fa", "#426ff5"]
-    };
     var colorScale = d3.scaleLinear()
-                .domain(d3.extent(coronaData, d => d[stat]))
+                .domain(d3.extent(dataCSV[currState].regions, d => d[stat]))
                 .range(colorValues[stat]);
-    // // draw map
+    // draw map
     var map = d3.select("#map")
         .selectAll('svg')
         .attr("width", width)
         .attr("height", height);
-    var projection = d3.geoMercator()
-                   .translate([width / 2, height / 2]);
-
+    // var projection = d3.geoMercator();
+    // projection.fitSize([width, height], topology);
+    // console.log(currState);
+    var projection = d3.geoAlbersUsa().fitSize([width, height], topology);
     var path = d3.geoPath()
         .projection(projection)
 
-    var bounds = d3.geoBounds(topology),
-        center = d3.geoCentroid(topology);
-
-    // Compute the angular distance between bound corners
-    var distance = d3.geoDistance(bounds[0], bounds[1]),
-        scale = (height + distance * 1000) / distance / Math.sqrt(2);
-    // Update the projection scale and centroid
-    projection.scale(scale).center(center);
     var atlas = map.selectAll('.map').data(topology.features);
-    atlas.exit().remove();
-    atlas.style("stroke", "black")
-        .attr("fill", d => {
-            if (d.properties[stat] == undefined) return '#ccc';
-            else return colorScale(d.properties[stat]);
-        });
     atlas.enter()
         .append("path")
         .classed('map',true)
@@ -124,17 +202,17 @@ const drawMap = function (coronaData, topology, stateDetails) {
             if (d.properties[stat] == undefined) return '#ccc';
             else return colorScale(d.properties[stat]);
         })
-        .on('click', function(d) {
-            d3.select(this).style('stroke', '#03fccf').style('stroke-width', 1);
-            if (prevSelected && prevSelected != this) d3.select(prevSelected).style('stroke', 'black');
-            prevSelected =this;
-            var info = d3.select("#county-info");
-            info.style('opacity', 1)
-                .html(`<p>County Name: ${d.properties.NAME}</p>
-                <p>Confirmed: ${d.properties.confirmed != undefined?d.properties.confirmed:'No data'}</p>
-                <p>Deaths: ${d.properties.deaths != undefined?d.properties.deaths:'No data'}</p>
-                <p>Recovered: ${d.properties.recovered != undefined?d.properties.recovered:'No data'}</p>`);
-        })
+        // .on('click', function(d) {
+        //     d3.select(this).style('stroke', '#03fccf').style('stroke-width', 1);
+        //     if (prevSelected && prevSelected != this) d3.select(prevSelected).style('stroke', 'black');
+        //     prevSelected =this;
+        //     var info = d3.select("#county-info");
+        //     info.style('opacity', 1)
+        //         .html(`<p>County Name: ${d.properties.NAME}</p>
+        //         <p>Confirmed: ${d.properties.confirmed != undefined?d.properties.confirmed:'No data'}</p>
+        //         <p>Deaths: ${d.properties.deaths != undefined?d.properties.deaths:'No data'}</p>
+        //         <p>Recovered: ${d.properties.recovered != undefined?d.properties.recovered:'No data'}</p>`);
+        // })
         .on('mouseover touchstart', function(d){
             var tooltip = d3.select('#tooltip');
             tooltip.style('opacity', 1)
@@ -150,11 +228,25 @@ const drawMap = function (coronaData, topology, stateDetails) {
         });
 }
 
-function showStateInfo(stateDetails) {
-    var stateInfo = d3.select("#state-info");
-    stateInfo.style('opacity', 1)
-        .html(`<p>State Name: ${stateDetails.name}</p>
-        <p>Confirmed: ${stateDetails.confirmed}</p>
-        <p>Deaths: ${stateDetails.deaths}</p>
-        <p>Recovered: ${stateDetails.recovered}</p>`);
+function drawMapWithStat() {
+    let stat = d3.select('#stat').property('value');
+    var colorScale = d3.scaleLinear()
+                .domain(d3.extent(dataCSV[currState].regions, d => d[stat]))
+                .range(colorValues[stat]);
+    console.log(colorScale);
+    let atlas = d3.select('#map').selectAll('svg').selectAll('.map');
+    atlas.style("stroke", "black")
+        .attr("fill", d => {
+            if (d.properties[stat] == undefined) return '#ccc';
+            else return colorScale(d.properties[stat]);
+        });
+}
+
+function showInfo(details) {
+    var info = d3.select("#state-info");
+    info.style('opacity', 1)
+        .html(`<p>Name: ${details.name}</p>
+        <p>Confirmed: ${details.confirmed}</p>
+        <p>Deaths: ${details.deaths}</p>
+        <p>Recovered: ${details.recovered}</p>`);
 }
